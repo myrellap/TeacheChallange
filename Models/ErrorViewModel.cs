ad17fd8afac09bf7da09d767c176c8c2a1c8915e
Myrella Putti <myrella.putti@aluno.senai.br>
2026-07-23 16:16:59 -0300

    Primeiro commit - Projeto o TeachChallenge criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TeacheChallange.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
